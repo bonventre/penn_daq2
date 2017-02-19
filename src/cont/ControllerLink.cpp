6bc46718ea40808d3fb24bc2452be9ac4ad22f32
hep <hep@daqbrick.(none)>
2017-02-19 12:00:29 -0500

    revert a change

--- src/cont/ControllerLink.cpp
+++ src/cont/ControllerLink.cpp
@@ -21,7 +21,6 @@
 #include "PedRun.h"
 #include "SeeReflection.h"
 #include "SeeReflectionEsum.h"
-#include "ScanReflections.h"
 #include "TriggerScan.h"
 #include "TTot.h"
 #include "VMon.h"
@@ -1230,33 +1229,6 @@ void *ControllerLink::ProcessCommand(void *arg)
       goto err;
     }
     SeeReflectionEsum(crateNum,slotMask,dacValue,frequency,update);
-    UnlockConnections(1,0x1<<crateNum);
-
-  }else if (strncmp(input,"scan_refl",9) == 0){
-    if (GetFlag(input,'h')){
-      lprintf("Usage: see_refl -c [crate num (int)] "
-          "-v [dac value (int)] -s [slot mask (hex)] "
-          "-t [trigger to enable (0-13)] -v [threshold dac] "
-          "-f [frequency (float)] -p [channel mask (hex)] "
-          "-d (update database)\n");
-      goto err;
-    }
-    int crateNum = GetInt(input,'c',2);
-    uint32_t slotMask = GetUInt(input,'s',0xFFFF);
-    uint32_t channelMask = GetUInt(input,'p',0xFFFFFFFF);
-    int triggerSelect = GetInt(input,'t',0);
-    int dacCounts = GetInt(input,'v',0);
-    float frequency = GetFloat(input,'f',20);
-    int update = GetFlag(input,'d');
-    int busy = LockConnections(1,0x1<<crateNum);
-    if (busy){
-      if (busy > 9)
-        lprintf("Trying to access a board that has not been connected\n");
-      else
-        lprintf("ThoseConnections are currently in use.\n");
-      goto err;
-    }
-    ScanReflection(crateNum,slotMask,channelMask,triggerSelect,dacCounts,frequency,update);
     UnlockConnections(1,0x1<<crateNum);
 
   }else if (strncmp(input,"trigger_scan",12) == 0){
